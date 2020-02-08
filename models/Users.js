90cd0ace458242861ef08f359ff1d6a45e93ff6f
Haidren <haidren.amalia@gmail.com>
2020-02-08 23:53:00 +0800

    Added MongoURI and ENVS

---
+++ models/Users.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    avatar:{
+        type: String
+    },
+    joinDate: {
+        type: Date,
+        default: Date.now
+    },
+    isActivated: {
+        type: Boolean,
+        default: false
+    }
+})
--- server.js
+++ server.js
@@ -1,32 +1,40 @@
 const { ApolloServer, gql } = require('apollo-server')
 
-const Todos = [
-    { task: "Wash Car", completed: false },
-    { task: "Fix Bulbs", completed: false }
-]
+const mongoose = require('mongoose')
+
+const dotenv = require('dotenv')
+
+dotenv.config({ path: 'variables.env' })
+
+mongoose
+    .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Database Connected!!');
+    })
+    .catch(err => {
+        console.error(err);
+    })
+
 
 const typeDefs = gql`
     type Todo {
-        tasks: String
+        task: String
         completed: Boolean
     }
 
     type Query {
         getTodos: [Todo]
     }
+
+    type Mutation {
+        addTodos(task: String, completed: Boolean) : Todo
+    }
 `
 
-const resolvers = {
-    Query: {
-        getTodos: function () {
-            return todos
-        }
-    }
-}
+
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers
+    typeDefs
 })
 
 server.listen().then(({url})=> {
