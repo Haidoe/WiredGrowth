4f9980bcf7d3746e87f781862ec9b075f0b08cca
Haidren <haidren.amalia@gmail.com>
2020-02-15 15:57:46 +0800

    Added Client

---
+++ client/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-content>
+    <v-container>
+      <h1>{{ sampleText }}</h1>
+    </v-container>
+  </v-content>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data: () => ({
+    sampleText: "This is your Home!"
+  })
+};
+</script>
