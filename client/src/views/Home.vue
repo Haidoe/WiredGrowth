5db8ed807e0c712e634f538a396792677f9b545e
Haidren <haidren.amalia@gmail.com>
2020-02-15 16:42:59 +0800

    Client - Added Apollo Boost and Vue Apollo and Sample GQL Query at Home Page

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,15 +2,36 @@
   <v-content>
     <v-container>
       <h1>{{ sampleText }}</h1>
+
+      <ApolloQuery :query="getCampusesQuery">
+        <template slot-scope="{ result: { loading, data } }">
+          <span v-if="loading">Loading...</span>
+          <ul v-else>
+            <li v-for="campus in data.getCampuses" :key="campus._id">
+              {{ campus.campus }}
+            </li>
+          </ul>
+        </template>
+      </ApolloQuery>
     </v-container>
   </v-content>
 </template>
 
 <script>
+import { gql } from "apollo-boost";
+
 export default {
   name: "Home",
   data: () => ({
-    sampleText: "This is your Home!"
+    sampleText: "This is your Home!",
+    getCampusesQuery: gql`
+      query {
+        getCampuses {
+          _id
+          campus
+        }
+      }
+    `
   })
 };
 </script>
