66926cf06b2b62ebfff9d55fca14cceb81760109
Haidren <haidren.istor@gmail.com>
2020-02-17 17:12:57 +0800

    Added Material Icons

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -1,8 +1,10 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify';
-import 'vuetify/dist/vuetify.min.css';
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 
-Vue.use(Vuetify);
+Vue.use(Vuetify)
 
 export default new Vuetify({
-});
+    iconfont: 'md'
+})
