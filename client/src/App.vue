abac8c9e34900bfbdb07cbea001cf48319e062b0
Haidren <haidren.istor@gmail.com>
2020-02-19 14:42:03 +0800

    Added Drawer Navigation

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,11 @@
 <template>
     <v-app>
-        <v-app-bar absolute color="primary" elevate-on-scroll dark v-if="user">
-            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-app-bar absolute color="primary" elevate-on-scroll dark>
+            <v-app-bar-nav-icon @click="drawer = !drawer" v-if="user"></v-app-bar-nav-icon>
 
-            <v-toolbar-title>Wired Growth</v-toolbar-title>
+            <v-toolbar-title>
+                <router-link to="/" class="toolbar-title">Wired Growth</router-link>
+            </v-toolbar-title>
 
             <v-spacer></v-spacer>
 
@@ -31,11 +33,54 @@
                 <router-view></router-view>
             </transition>
         </main>
+
+        <v-navigation-drawer v-model="drawer" absolute temporary v-show="user">
+            <v-list-item>
+                <v-list-item-avatar>
+                    <v-img :src="activeUser.avatar"></v-img>
+                </v-list-item-avatar>
+
+                <v-list-item-content>
+                    <v-list-item-title>{{ activeUser.username }}</v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+
+            <v-divider></v-divider>
+
+            <v-list dense>
+                <v-list-item
+                    v-for="item in drawerNavList"
+                    :key="`${item.title}-drawer`"
+                    :to="item.path"
+                    link
+                >
+                    <v-list-item-icon>
+                        <v-icon>{{ item.icon }}</v-icon>
+                    </v-list-item-icon>
+
+                    <v-list-item-content>
+                        <v-list-item-title>{{ item.title }}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-list-item link @click="handleSignoutUser">
+                    <v-list-item-icon>
+                        <v-icon>exit_to_app</v-icon>
+                    </v-list-item-icon>
+
+                    <v-list-item-content>
+                        <v-list-item-title>Sign out</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
     </v-app>
 </template>
 <script>
 import { mapGetters, mapActions } from "vuex";
 export default {
+    data: () => ({
+        drawer: false
+    }),
     computed: {
         ...mapGetters(["user"]),
         navList() {
@@ -44,7 +89,7 @@ export default {
                     {
                         path: "/attendance",
                         title: "Attendance",
-                        icon: "person"
+                        icon: "calendar_today"
                     }
                 ];
             }
@@ -54,13 +99,57 @@ export default {
                     path: "/signin",
                     title: "Sign in",
                     icon: "mdi-lock-open"
+                }
+                // {
+                //     path: "/signup",
+                //     title: "Sign up",
+                //     icon: "edit"
+                // }
+            ];
+        },
+        drawerNavList() {
+            return [
+                {
+                    path: "/attendance",
+                    title: "Attendance",
+                    icon: "calendar_today"
                 },
                 {
-                    path: "/signup",
-                    title: "Sign up",
-                    icon: "edit"
+                    path: "/attendee",
+                    title: "Attendee",
+                    icon: "person"
+                },
+                {
+                    path: "/team",
+                    title: "Team",
+                    icon: "group"
+                },
+                {
+                    path: "/task",
+                    title: "Task",
+                    icon: "work"
+                },
+                {
+                    path: "/campus",
+                    title: "Campus",
+                    icon: "location_on"
+                },
+                {
+                    path: "/attendance-status",
+                    title: "Attendance Status",
+                    icon: "emoji_emotions"
                 }
             ];
+        },
+        activeUser() {
+            if (this.user) {
+                return this.user;
+            } else {
+                return {
+                    username: "John Doe",
+                    avatar: "https://randomuser.me/api/portraits/men/78.jpg"
+                };
+            }
         }
     },
     methods: {
@@ -83,5 +172,9 @@ export default {
 .fade-enter,
 .fade-leave-active {
     opacity: 0;
+}
+.toolbar-title {
+    color: #fff !important;
+    text-decoration: none;
 }
 </style>
